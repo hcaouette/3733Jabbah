98a462dabfd3ecb10ba40ef43c74f99ab17d04b0
Peter <pjjankowski@sizzlescene.com>
2018-12-06 10:36:45 -0500

    deleteschedule test complete

--- src/test/java/jabbah/controllers/DeleteScheduleTest.java
+++ src/test/java/jabbah/controllers/DeleteScheduleTest.java
@@ -1,6 +1,17 @@
 package jabbah.controllers;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.google.gson.Gson;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
@@ -9,35 +20,50 @@ public class DeleteScheduleTest {
 
     private final String CONTENT_TYPE = "image/jpeg";
     private S3Event event;
-/*
-    @Before
-    public void setUp() throws IOException {
-        event = TestUtils.parse("/s3-event.put.json", S3Event.class);
-
-        // TODO: customize your mock logic for s3 client
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(CONTENT_TYPE);
-        when(s3Object.getObjectMetadata()).thenReturn(objectMetadata);
-        when(s3Client.getObject(getObjectRequest.capture())).thenReturn(s3Object);
-    }
-
-    private Context createContext() {
+    Context createContext(String apiCall) {
         TestContext ctx = new TestContext();
-
-        // TODO: customize your context here if needed.
-        ctx.setFunctionName("Your Function Name");
-
+        ctx.setFunctionName(apiCall);
         return ctx;
     }
 
     @Test
-    public void testDeleteSchedule() {
-        DeleteSchedule handler = new DeleteSchedule(s3Client);
-        Context ctx = createContext();
+    public void testDeleteSchedule() throws IOException {
+        CreateSchedule handler = new CreateSchedule();
+        DeleteSchedule handlerTwo = new DeleteSchedule();
 
-        String output = handler.handleRequest(event, ctx);
+        int rnd = (int) (Math.random() * 10000000);
+        int rndTwo = (int) (Math.random() * 10000000);
+        String rndS = Integer.toString(rnd);
+        String rndSTwo = Integer.toString(rndTwo);
+        CreateScheduleRequest ar = new CreateScheduleRequest(rndS, "12:12", "12:27", 15, "1888-08-08", "1888-09-09", "zimm", 1, rndSTwo);
 
-        // TODO: validate output here if needed.
-        Assert.assertEquals(CONTENT_TYPE, output);
-    } */
+        DeleteScheduleRequest arTwo = new DeleteScheduleRequest(rndS);
+        String ccRequest = new Gson().toJson(ar);
+        String jsonRequest = new Gson().toJson(new PostRequest(ccRequest));
+
+        InputStream input = new ByteArrayInputStream(jsonRequest.getBytes());
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, createContext("create"));
+
+        PostResponse post = new Gson().fromJson(output.toString(), PostResponse.class);
+        CreateScheduleResponse resp = new Gson().fromJson(post.body, CreateScheduleResponse.class);
+        //System.out.println(resp);
+
+        String ccRequestTwo = new Gson().toJson(arTwo);
+        String jsonRequestTwo = new Gson().toJson(new PostRequest(ccRequestTwo));
+
+        InputStream inputTwo = new ByteArrayInputStream(jsonRequest.getBytes());
+        OutputStream outputTwo = new ByteArrayOutputStream();
+
+        handlerTwo.handleRequest(inputTwo, outputTwo, createContext("delete"));
+
+        PostResponse postTwo = new Gson().fromJson(outputTwo.toString(), PostResponse.class);
+        DeleteScheduleResponse respTwo = new Gson().fromJson(postTwo.body, DeleteScheduleResponse.class);
+        System.out.println(respTwo);
+
+        Assert.assertEquals("Successfully deleted schedule:" + rndS, respTwo.response);
+
+        //resp = new DeleteScheduleResponse("Successfully deleted schedule:" + rndS, resp.response);
+    }
 }
