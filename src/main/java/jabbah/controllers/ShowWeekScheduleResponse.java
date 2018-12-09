a801a2613fcb3b4aca0924ee781b62960045d974
Hunter <huntercaouette@gmail.com>
2018-12-09 12:28:45 -0500

    pullSchedule tentatively working

--- src/main/java/jabbah/controllers/ShowWeekScheduleResponse.java
+++ src/main/java/jabbah/controllers/ShowWeekScheduleResponse.java
@@ -7,17 +7,17 @@ import java.util.List;
 public class ShowWeekScheduleResponse {
 	List<DaysInSchedule> week;
 	int httpCode;
-	
+
 	public ShowWeekScheduleResponse(List<DaysInSchedule> week, int code) {
 		this.week = week;
 		this.httpCode = code;
 	}
-	
+
 	public ShowWeekScheduleResponse(int code) {
 		this.week = new ArrayList<DaysInSchedule>();
 		this.httpCode = code;
 	}
-	
+
 	public String toString() {
 		if(week == null)
 			return "Given date is not within this Schedule's boundaries";
